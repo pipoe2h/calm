02318502f401e5810d2918ee752e276373f101d2
Jose Gomez <pipoe2h@gmail.com>
2020-04-05 17:45:12 +0100

    Update: simplify payload

--- demo/nsx-t/tenant_segment.py
+++ demo/nsx-t/tenant_segment.py
@@ -3,79 +3,103 @@ nsx_password = '@@{nsx_password}@@'
 nsx_ip = '@@{nsx_mgr_ip}@@'
 tenant_uuid = '@@{TENANT_UUID}@@'
 segment_name = '@@{segment_name}@@'
+nsx_ipam_gw = '@@{defaultGwIp}@@'
+nsx_ipam_range = '@@{dhcp_range}@@'
+nsx_ipam_cidr = '@@{network_cidr}@@'
+nsx_tz_uuid = '@@{tzUuid}@@'
 
 headers = {'Content-Type': 'application/json',  'Accept':'application/json'}
 
+# payload = {
+#     "resource_type": "Infra",
+#     "id": "infra",
+#     "children": [
+#       {
+#         "Tier1": {
+#           "tier0_path": "/infra/tier-0s/{}".format(nsx_tier0_id),
+#           "failover_mode": "NON_PREEMPTIVE",
+#           "dhcp_config_paths": [
+#               "/infra/dhcp-server-configs/{}".format(nsx_dhcp)
+#           ],
+#           "force_whitelisting": "false",
+#           "default_rule_logging": "false",
+#           "disable_firewall": "false",
+#           "resource_type": "Tier1",
+#           "id": "{}-tier-1-gw".format(tenant_uuid),
+#           "description": "{}-tier-1-gw".format(tenant_uuid),
+#           "display_name": "{}-tier-1-gw".format(tenant_uuid),
+#           "route_advertisement_types": [
+#               "TIER1_CONNECTED",
+#               "TIER1_STATIC_ROUTES"
+#           ],
+#           "tags": [
+#             {"scope": "calm", "tag": "{}".format(tenant_uuid)}
+#           ],
+#           "children": [
+#             { 
+#                 "resource_type": "ChildLocaleServices",
+#                 "LocaleServices":{ 
+#                    "resource_type": "LocaleServices",
+#                    "id": "{}".format(nsx_edge_cluster_uuid),
+#                    "edge_cluster_path": "/infra/sites/default/enforcement-points/default/edge-clusters/{}".format(nsx_edge_cluster_uuid)
+#                 }
+#              },
+#             {
+#                 "Segment": {
+#                   "subnets": [
+#                     {
+#                       "gateway_address": "{}".format(nsx_ipam_gw),
+#                       "dhcp_ranges": [
+#                           "{}".format(nsx_ipam_range)
+#                       ],
+#                       "network": "{}".format(nsx_ipam_cidr)
+#                     }
+#                   ],
+#                   "resource_type": "Segment",
+#                   "id": "{}-{}".format(tenant_uuid,segment_name),
+#                   "description": "{}-{}".format(tenant_uuid,segment_name),
+#                   "display_name": "{}-{}".format(tenant_uuid,segment_name),
+#                   "transport_zone_path": "/infra/sites/default/enforcement-points/default/transport-zones/{}".format(nsx_tz_uuid),
+#                   "tags": [
+#                     {"scope": "calm", "tag": "{}".format(tenant_uuid)}
+#                   ],
+#                   "marked_for_delete": "false"
+#                 },
+#                 "resource_type": "ChildSegment",
+#                 "marked_for_delete": "false"
+#             }
+#           ],
+#           "marked_for_delete": "false"
+#         },
+#         "resource_type": "ChildTier1",
+#         "marked_for_delete": "false"
+#       }
+#     ],
+#     "marked_for_delete": "false",
+#     "connectivity_strategy": "WHITELIST"
+# }
+
 payload = {
-    "resource_type": "Infra",
-    "id": "infra",
-    "children": [
+    "subnets": [
       {
-        "Tier1": {
-          "tier0_path": "/infra/tier-0s/{}".format(nsx_tier0_id),
-          "failover_mode": "NON_PREEMPTIVE",
-          "dhcp_config_paths": [
-              "/infra/dhcp-server-configs/{}".format(nsx_dhcp)
-          ],
-          "force_whitelisting": "false",
-          "default_rule_logging": "false",
-          "disable_firewall": "false",
-          "resource_type": "Tier1",
-          "id": "{}-tier-1-gw".format(tenant_uuid),
-          "description": "{}-tier-1-gw".format(tenant_uuid),
-          "display_name": "{}-tier-1-gw".format(tenant_uuid),
-          "route_advertisement_types": [
-              "TIER1_CONNECTED",
-              "TIER1_STATIC_ROUTES"
-          ],
-          "tags": [
-            {"scope": "calm", "tag": "{}".format(tenant_uuid)}
-          ],
-          "children": [
-            { 
-                "resource_type": "ChildLocaleServices",
-                "LocaleServices":{ 
-                   "resource_type": "LocaleServices",
-                   "id": "{}".format(nsx_edge_cluster_uuid),
-                   "edge_cluster_path": "/infra/sites/default/enforcement-points/default/edge-clusters/{}".format(nsx_edge_cluster_uuid)
-                }
-             },
-            {
-                "Segment": {
-                  "subnets": [
-                    {
-                      "gateway_address": "{}".format(nsx_ipam_gw),
-                      "dhcp_ranges": [
-                          "{}".format(nsx_ipam_range)
-                      ],
-                      "network": "{}".format(nsx_ipam_cidr)
-                    }
-                  ],
-                  "resource_type": "Segment",
-                  "id": "{}-{}".format(tenant_uuid,segment_name),
-                  "description": "{}-{}".format(tenant_uuid,segment_name),
-                  "display_name": "{}-{}".format(tenant_uuid,segment_name),
-                  "transport_zone_path": "/infra/sites/default/enforcement-points/default/transport-zones/{}".format(nsx_tz_uuid),
-                  "tags": [
-                    {"scope": "calm", "tag": "{}".format(tenant_uuid)}
-                  ],
-                  "marked_for_delete": "false"
-                },
-                "resource_type": "ChildSegment",
-                "marked_for_delete": "false"
-            }
-          ],
-          "marked_for_delete": "false"
-        },
-        "resource_type": "ChildTier1",
-        "marked_for_delete": "false"
+        "gateway_address": "{}".format(nsx_ipam_gw),
+        "dhcp_ranges": [
+            "{}".format(nsx_ipam_range)
+        ],
+        "network": "{}".format(nsx_ipam_cidr)
       }
     ],
-    "marked_for_delete": "false",
-    "connectivity_strategy": "WHITELIST"
+    "resource_type": "Segment",
+    "description": "{}-{}".format(tenant_uuid,segment_name),
+    "display_name": "{}-{}".format(tenant_uuid,segment_name),
+    "transport_zone_path": "/infra/sites/default/enforcement-points/default/transport-zones/{}".format(nsx_tz_uuid),
+    "tags": [
+      {"scope": "calm", "tag": "{}".format(tenant_uuid)}
+    ],
+    "marked_for_delete": "false"
 }
 
-api_action = '/policy/api/v1/infra'
+api_action = '/policy/api/v1/infra/tier-1s/{}-tier-1-gw/segments/{}-{}'.format(tenant_uuid,tenant_uuid,segment_name)
 url = 'https://{}{}'.format(
     nsx_ip,
     api_action
