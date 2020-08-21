c7a2241f587426a43ef5fffd3f45c813f0671ab5
Jose Gomez <pipoe2h@gmail.com>
2020-08-21 17:47:38 +0100

    Added: Terraform demo

---
+++ demo/terraform/main.tf
@@ -0,0 +1,26 @@
+# Configure Nutanix Objects as backend for Terraform state files https://www.terraform.io/docs/backends/types/s3.html
+terraform {
+  backend "s3" {
+    bucket = "terraform"
+    endpoint = "http://192.168.2.78"
+    key = "demo/terraform.tfstate"
+    region = "us-east-1"
+    profile = "objects"
+#    shared_credentials_file = "./app-env"
+    skip_credentials_validation = true
+#    skip_metadata_api_check = true
+#    skip_region_validation = true
+    force_path_style = true
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 3.0"
+  region  = var.aws_region
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = var.vpc_cidr
+}
---
+++ demo/terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+    default = "eu-west-2"
+}
+
+variable "vpc_cidr" {
+    default = "10.20.0.0/16"
+}
